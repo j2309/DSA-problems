a7349c7d4ca049aa42c8996fef0ac3dbbd65bae3
Janhvi Mohurle <146566657+j2309@users.noreply.github.com>
2026-02-02 15:36:11 +0530

    Update secondlargest.py

--- secondlargest.py
+++ secondlargest.py
@@ -3,4 +3,20 @@ def secondlargest(numbers): #1.make function
     unique_num=list(set(numbers)) #2. remove duplicates by converting the set to the list 
     unique_num.sort() #3. sort the list small to large
     return unique_num[-2]#4.return the second largest number
+
 print(secondlargest([10,20,30,40,50,60,70,80,40,55,60])) #5.print it
+
+
+
+
+# using largest variable 
+def secondlargest(self,nums):
+    largest=nums[0]
+    second_largest=-1
+    for num in nums:
+        if num>largest:
+            second_largest =largest
+            num=largest
+       elif num >second_largest and num!=largest:
+           num = second largest
+  return second_largest
