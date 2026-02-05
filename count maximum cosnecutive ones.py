3e6f491b6b9fc98020e93007b4d5300a8525ec85
Janhvi Mohurle <146566657+j2309@users.noreply.github.com>
2026-02-05 12:02:50 +0530

    Create count maximum cosnecutive ones.py

---
+++ count maximum cosnecutive ones.py
@@ -0,0 +1,10 @@
+# to find the maximum frequency occuring of ones in an given array
+def count_maxones():
+int counter=0,int max=0
+for i inn range( :arr.size()):
+  if arr[i]==1:
+    counter++
+    max= max(max,count)
+  else:
+    counter = 0
+return max
