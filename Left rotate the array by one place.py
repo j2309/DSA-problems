71fb4e22890b68f44d44e7b725cf55a67adf9cd4
Janhvi Mohurle <146566657+j2309@users.noreply.github.com>
2026-02-21 00:19:16 +0530

    Create Left rotate the array by one place.py

---
+++ Left rotate the array by one place.py
@@ -0,0 +1,8 @@
+#1. brute idea-create an empty array and solve using it. 
+OR 
+#2 . Optimal idea- create temporary variable. TC:O(n),SC:o(1)
+def left_rotate_the_array_by_one(self, nums):
+ temp =arr[0] 
+ for i in range(1:0):
+   arr[i-1]=arr[i]
+   arr[n-1]=temp
