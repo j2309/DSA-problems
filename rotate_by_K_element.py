fd953db38d677f22cac0fe64431ce11da75f33d7
Janhvi Mohurle <146566657+j2309@users.noreply.github.com>
2026-02-21 00:33:11 +0530

    Create rotate_by_K_element.py

---
+++ rotate_by_K_element.py
@@ -0,0 +1,15 @@
+# rotate the array by K elements
+def rotatebyK(self, nums):
+  temp =arr[0:2] # create one temp array and store first three index values.
+  for i in range [3:]: # i loop starts from fourth element
+    arr[i-k]=a[i]
+  for j in range[4:n-3]:# j loop - swap the element in first 
+    arr[i]=temp[j]
+    j++
+
+OR
+
+for i in range[n-d:n]
+arr[i]=temp[i-(n-d)]
+    
+    
